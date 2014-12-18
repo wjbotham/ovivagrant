c2fdc091e0b532bae71c72c6e63cc1d6ee6c3ef1
Wesley Botham <wjbotham@gmail.com>
2014-12-18 13:51:38 -0600

    I don't want to talk about it

--- bootstrap/setup.sh
+++ bootstrap/setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 openvas-mkcert -q
+
+# If you remove this line which installs vim, the openvas-nvt-sync will fail.
+# No, I don't know why. Yes, I tried installing each of vim's dependencies
+# one by one to see if one of them is a hidden dependency. Yes, I realize this
+# reflects poorly on me as a developer.
+apt-get install -y vim
 openvas-nvt-sync
