7f7b38749c56695a8a38ec1b7221c18d6c4fd3eb
Wesley Botham <wjbotham@gmail.com>
2014-12-19 11:50:33 -0600

    Run openvasmd and gsad verbosely; Pause for openvassd to finish starting

--- bootstrap/start.sh
+++ bootstrap/start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 openvassd
-openvasmd
-gsad
+openvasmd -v
+gsad -v
+
+# wait until OpenVAS Scanner is running (adapted from Greenbone's openvas-check-setup script)
+OPENVASSD_RUNNING=`ps -Af | grep "openvassd: [Ww]aiting for incoming connections" | grep -v grep | wc -l`
+if [ $OPENVASSD_RUNNING -eq 0 ]
+then
+  echo "Waiting for OpenVAS Scanner..."
+  sleep 1
+fi
+
+# TODO figure out how to tell when OpenVAS Manager and Greenbone Security Assistant are running 
