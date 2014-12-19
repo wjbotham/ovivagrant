4c414d9ebce9d41e98a64c7d2feba8558d7d5add
Wesley Botham <wjbotham@gmail.com>
2014-12-19 15:26:14 -0600

    This never worked
    First because I used an if instead of a while
    Second because it loops infinitely if I use a while anyway

--- bootstrap/start.sh
+++ bootstrap/start.sh
@@ -8,13 +8,3 @@ apt-get install -y vim
 openvassd
 openvasmd -v
 gsad -v
-
-# wait until OpenVAS Scanner is running (adapted from Greenbone's openvas-check-setup script)
-OPENVASSD_RUNNING=`ps -Af | grep "openvassd: [Ww]aiting for incoming connections" | grep -v grep | wc -l`
-if [ $OPENVASSD_RUNNING -eq 0 ]
-then
-  echo "Waiting for OpenVAS Scanner..."
-  sleep 1
-fi
-
-# TODO figure out how to tell when OpenVAS Manager and Greenbone Security Assistant are running 
