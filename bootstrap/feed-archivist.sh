f2cdf2db9006094e7f83815c6e629a69e96a8755
Wesley Botham <wjbotham@gmail.com>
2014-12-19 11:51:14 -0600

    Don't store CERT backups; Different archive directories; Log how much time archiving takes
    CERT syncing is really fast and has some complications I haven't figured
    out, so I figure archiving CERT wasn't necessary.

--- bootstrap/feed-archivist.sh
+++ bootstrap/feed-archivist.sh
@@ -3,38 +3,30 @@
 OPENVAS_DIR=/usr/local/var/lib/openvas
 VAG_DIR=/vagrant
 NVT=$OPENVAS_DIR/plugins
-NVT_TAR=nvt-feed-backup.tar.gz
+NVT_TAR=$VAG_DIR/nvt-feed-backup.tar.gz
 SCAP=$OPENVAS_DIR/scap-data
-SCAP_TAR=scap-feed-backup.tar.gz
-CERT=$OPENVAS_DIR/cert-data
-CERT_TAR=vagrant/cert-feed-backup.tar.gz
+SCAP_TAR=$VAG_DIR/scap-feed-backup.tar.gz
 
 if [ "$1" = "restore" ]
 then
   echo "Checking for feed data to restore from archive..."
   # If OpenVAS is missing data for one of the three feeds and we have an archive
   # for that feed, then we give it to OpenVAS.
-  if [[ -f $VAG_DIR/$NVT_TAR && ! -f $NVT/plugin_feed_info.inc ]]
+  if [[ -f $NVT_TAR && ! -f $NVT/plugin_feed_info.inc ]]
   then
     echo "Restoring NVT..."
-    tar -zxvf $VAG_DIR/$NVT_TAR -C /
+    tar -zxf $NVT_TAR -C /
   fi
-  if [[ -f $VAG_DIR/$SCAP_TAR && ! -d $SCAP ]]
+  if [[ -f $SCAP_TAR && ! -d $SCAP ]]
   then
     echo "Restoring SCAP..."
-    tar -zxvf $VAG_DIR/$SCAP_TAR -C /
-  fi
-  if [[ -f $VAG_DIR/$CERT_TAR && ! -d $CERT ]]
-  then
-    echo "Restoring CERT..."
-    tar -zxvf $VAG_DIR/$CERT_TAR -C /
+    tar -zxf $SCAP_TAR -C /
   fi
 elif [ "$1" = "save" ]
 then
   echo "Archiving and compressing the updated feed data..."
-  tar -zcf $NVT_TAR $NVT
-  tar -zcf $SCAP_TAR $SCAP
-  tar -zcf $CERT_TAR $CERT
+  { time tar -zcf $NVT_TAR  $NVT  ; } 2> /vagrant/nvt_tar_time.log
+  { time tar -zcf $SCAP_TAR $SCAP ; } 2> /vagrant/scap_tar_time.log
 else
   echo "Feed archive script does not recognize this argument: $1"
 fi
