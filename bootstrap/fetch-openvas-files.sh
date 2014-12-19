b1d6212f0d02bd325ebfde0991bece3019b3ba84
Wesley Botham <wjbotham@gmail.com>
2014-12-19 15:03:30 -0600

    Keep source files around so we don't need to download them again

--- bootstrap/fetch-openvas-files.sh
+++ bootstrap/fetch-openvas-files.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-cd /home/vagrant
+cd /vagrant
 
-wget http://wald.intevation.org/frs/download.php/1833/openvas-libraries-7.0.6.tar.gz
-wget http://wald.intevation.org/frs/download.php/1844/openvas-scanner-4.0.5.tar.gz
-wget http://wald.intevation.org/frs/download.php/1849/openvas-manager-5.0.7.tar.gz
-wget http://wald.intevation.org/frs/download.php/1799/greenbone-security-assistant-5.0.4.tar.gz
-wget http://wald.intevation.org/frs/download.php/1803/openvas-cli-1.3.1.tar.gz
+if [ ! -f openvas-libraries-7.0.6.tar.gz ]
+then
+  wget http://wald.intevation.org/frs/download.php/1833/openvas-libraries-7.0.6.tar.gz
+fi
+if [ ! -f openvas-scanner-4.0.5.tar.gz ]
+then
+  wget http://wald.intevation.org/frs/download.php/1844/openvas-scanner-4.0.5.tar.gz
+fi
+if [ ! -f openvas-manager-5.0.7.tar.gz ]
+then
+  wget http://wald.intevation.org/frs/download.php/1849/openvas-manager-5.0.7.tar.gz
+fi
+if [ ! -f greenbone-security-assistant-5.0.4.tar.gz ]
+then
+  wget http://wald.intevation.org/frs/download.php/1799/greenbone-security-assistant-5.0.4.tar.gz
+fi
+if [ ! -f openvas-cli-1.3.1.tar.gz ]
+then
+  wget http://wald.intevation.org/frs/download.php/1803/openvas-cli-1.3.1.tar.gz
+fi
 
 CHECKSUMS=/vagrant/checksums/*
 for SHA_FILE in $CHECKSUMS
@@ -16,6 +31,5 @@ do
   CURRENT_SHA=$(openssl dgst -sha1 $TARGET_FILE | awk '{print $2}')
   [ "$ORIGINAL_SHA" != "$CURRENT_SHA" ] && { echo "ERROR: $SHA_FILE did not match SHA1 of $TARGET_FILE!"; exit 1; }
   tar xzf $TARGET_FILE
-  rm $TARGET_FILE
 done
 echo "!!!!!! All SHA1 fields matched !!!!!!"
