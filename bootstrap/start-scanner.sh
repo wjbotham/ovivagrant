2460efe5e298c5e06738dd99f9d03496a35393be
Wesley Botham <wjbotham@gmail.com>
2014-12-22 13:43:09 -0600

    Make scanner startup progress updates less annoying

--- bootstrap/start-scanner.sh
+++ bootstrap/start-scanner.sh
@@ -7,5 +7,6 @@ openvassd
 while [ `ps -aef | grep openvassd | grep "Waiting for incoming connections" | wc -l` -eq 0 ]
 do
   ps -aef | grep openvassd | grep Reloaded | awk '{for(i=8;i<NF;i++)printf "%s",$i OFS; if (NF) printf "%s",$NF; printf ORS}'
-  sleep 10
+  sleep 30
 done
+echo "OpenVAS Scanner is running!"
