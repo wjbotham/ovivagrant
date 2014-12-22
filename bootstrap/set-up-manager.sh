af8605adfe8a4c2b1b4b4bd699ab2e2503ef84e2
Wesley Botham <wjbotham@gmail.com>
2014-12-22 14:17:57 -0600

    Should be a full startup process now~!

---
+++ bootstrap/set-up-manager.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+openvasmd --rebuild --progress
--- bootstrap/start-scanner.sh
+++ bootstrap/start-scanner.sh
@@ -7,6 +7,11 @@ openvassd
 while [ `ps -aef | grep openvassd | grep "Waiting for incoming connections" | wc -l` -eq 0 ]
 do
   ps -aef | grep openvassd | grep Reloaded | awk '{for(i=8;i<NF;i++)printf "%s",$i OFS; if (NF) printf "%s",$NF; printf ORS}'
+  if [ `ps -aef | grep openvas | grep openvassd | wc -l` -eq 0 ]
+  then
+    echo "The openvassd process disappeared! Exiting."
+    exit 1
+  fi
   sleep 30
 done
 echo "OpenVAS Scanner is running!"
