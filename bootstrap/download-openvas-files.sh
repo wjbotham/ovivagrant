f604f203d0f719375681efc8ece2b23f1f25a350
Wesley Botham <wjbotham@gmail.com>
2015-01-02 09:06:32 -0800

    Neater code for downloading deps

--- bootstrap/download-openvas-files.sh
+++ bootstrap/download-openvas-files.sh
@@ -6,34 +6,25 @@ then
 fi
 cd /vagrant/downloads
 
-if [ ! -f openvas-libraries-7.0.6.tar.gz ]
-then
-  wget http://wald.intevation.org/frs/download.php/1833/openvas-libraries-7.0.6.tar.gz
-fi
-if [ ! -f openvas-scanner-4.0.5.tar.gz ]
-then
-  wget http://wald.intevation.org/frs/download.php/1844/openvas-scanner-4.0.5.tar.gz
-fi
-if [ ! -f openvas-manager-5.0.7.tar.gz ]
-then
-  wget http://wald.intevation.org/frs/download.php/1849/openvas-manager-5.0.7.tar.gz
-fi
-if [ ! -f greenbone-security-assistant-5.0.4.tar.gz ]
-then
-  wget http://wald.intevation.org/frs/download.php/1799/greenbone-security-assistant-5.0.4.tar.gz
-fi
-if [ ! -f openvas-cli-1.3.1.tar.gz ]
-then
-  wget http://wald.intevation.org/frs/download.php/1803/openvas-cli-1.3.1.tar.gz
-fi
-
-CHECKSUMS=/vagrant/checksums/*
-for SHA_FILE in $CHECKSUMS
+deps=(
+  "openvas-libraries-7.0.6|1833"
+  "openvas-scanner-4.0.5|1844"
+  "openvas-manager-5.0.7|1849"
+  "greenbone-security-assistant-5.0.4|1799"
+  "openvas-cli-1.3.1|1803"
+)
+for dep in ${deps[@]}
 do
-  ORIGINAL_SHA=$(awk '{print $1}' $SHA_FILE)
-  TARGET_FILE=$(awk '{print $2}' $SHA_FILE)
-  CURRENT_SHA=$(openssl dgst -sha1 $TARGET_FILE | awk '{print $2}')
-  [ "$ORIGINAL_SHA" != "$CURRENT_SHA" ] && { echo "ERROR: $SHA_FILE did not match SHA1 of $TARGET_FILE!"; exit 1; }
-  tar xzf $TARGET_FILE -C /home/vagrant
+  name=`echo $dep | cut -f 1 -d '|'`
+  id=`echo $dep | cut -f 2 -d '|'`
+  if [ ! -f $name.tar.gz ]
+  then
+    wget http://wald.intevation.org/frs/download.php/$id/$name.tar.gz
+  fi
+  ORIGINAL_SHA=$(awk '{print $1}' /vagrant/checksums/$name.tar.gz.sha1)
+  CURRENT_SHA=$(openssl dgst -sha1 $name.tar.gz | awk '{print $2}')
+  [ "$ORIGINAL_SHA" != "$CURRENT_SHA" ] && { echo "ERROR: $name did not have matching SHA1!"; exit 1; }
+  tar xzf $name.tar.gz -C /home/vagrant
 done
+
 echo "!!!!!! All SHA1 fields matched !!!!!!"
