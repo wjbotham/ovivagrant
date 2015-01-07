2161d8e6a96a88da957b6d85ec4352e2c67c987e
Wesley Botham <wjbotham@gmail.com>
2015-01-07 11:37:31 -0600

    Turn off GSA logging so it doesn't kill itself

--- bootstrap/start-manager-and-gsa.sh
+++ bootstrap/start-manager-and-gsa.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 openvasmd -v
+
+# This line turns off Greenbone's logging because it causes the following
+# error that constantly kills Greenbone while I'm working with it:
+#   g_string_erase: assertion `pos + len <= string->len' failed
+sed -i 's/level=[0-9]\+/level=0/' /usr/local/etc/openvas/gsad_log.conf
 gsad -v
