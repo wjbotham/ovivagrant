451db9c7df7f18e0be2b57d99fdacf6f7f653a35
Wesley Botham <wjbotham@gmail.com>
2015-07-13 17:10:59 -0500

    Add some extra log lines when provision scripts start

--- bootstrap/start-manager-and-gsa.sh
+++ bootstrap/start-manager-and-gsa.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+echo "Starting OpenVAS Manager..."
 openvasmd -v
 
 # This line turns off Greenbone's logging because it causes the following
 # error that constantly kills Greenbone while I'm working with it:
 #   g_string_erase: assertion `pos + len <= string->len' failed
 sed -i 's/level=[0-9]\+/level=0/' /usr/local/etc/openvas/gsad_log.conf
+echo "Starting Greenbone..."
 gsad -v
