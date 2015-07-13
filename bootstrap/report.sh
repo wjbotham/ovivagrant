28fbb5629f7e11702b93260862e0b167a6e2bdea
Wesley Botham <wjbotham@gmail.com>
2015-07-13 15:59:22 -0500

    Instruct user to set their admin password

--- bootstrap/report.sh
+++ bootstrap/report.sh
@@ -2,3 +2,6 @@
 
 bash /vagrant/openvas-check-setup
 echo `ps -ef | grep openvas`
+
+echo 'Please run `vagrant ssh` and set your Greenbone admin password:'
+echo '  openvasmd --user=admin --new-password=<YOUR CHOSEN PASSWORD>'
